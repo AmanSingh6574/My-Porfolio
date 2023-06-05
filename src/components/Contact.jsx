47312fc810525c8de6e6dea7f1de5485dc6b0a4e
Aman Singh <amansingh6574@gmail.com>
2023-06-05 18:04:21 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,13 @@
 function Contact() {
   return (
-    <div name="Contact" className=" w-full ">
+    <div name="Contact" className=" w-full min-h-screen">
       <div className="max-w-screen-lg mx-auto min-h-screen flex flex-col justify-center items-center  sm:mb-18 p-4">
     
         <h1 className=" mt-9 text-4xl font-bold w-80 mb-10 border-b-4 border-gray-500">Contact details</h1>
         
         <div className="form w-[350px] sm:w-[489px] mb-[200px] sm:mb-20">
-          <h3 className="font-semibold text-xl">GET IN TOUCH</h3>
-          <h1 className="font-bold text-4xl ">Contact.</h1>
+          <h3 className="font-semibold text-gray-700 text-xl text-center">GET IN TOUCH</h3>
+          <h1 className="font-bold text-4xl text-center">Contact.</h1>
 
           <form  action="https://getform.io/f/8b1e055d-3ca6-480d-a03a-146e51d26c50"  method="POST">
             <label htmlFor="">
@@ -42,7 +42,7 @@ function Contact() {
               ></textarea>
             </label>
 
-            <div>
+            <div className="text-center">
               <button className="btn hover:scale-105 duration-200">Send</button>
             </div>
           </form>
