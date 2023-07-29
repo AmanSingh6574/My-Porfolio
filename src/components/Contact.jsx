98b6367ebac6048db17f155860b09e51ffc9240f
AmanSingh6574 <amansingh6574@gmail.com>
2023-07-30 00:25:04 +0530

    changes made

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,36 +1,46 @@
+import React from "react";
+
 function Contact() {
   return (
-    <div name="Contact" className=" w-full min-h-screen">
-      <div className="max-w-screen-lg mx-auto min-h-screen flex flex-col justify-center items-center  sm:mb-18 p-4">
-    
-        <h1 className=" mt-9 text-4xl font-bold w-80 mb-10 border-b-4 border-gray-500">Contact details</h1>
-        
-        <div className="form w-[350px] sm:w-[489px] mb-[200px] sm:mb-20">
-          <h3 className="font-semibold text-gray-700 text-xl text-center">GET IN TOUCH</h3>
+    <div name="Contact" className="w-full min-h-screen">
+      <div className="max-w-screen-lg mx-auto min-h-screen flex flex-col justify-center items-center sm:mb-18 p-4">
+        <h1 className="mt-9 text-4xl font-bold w-80 mb-10 border-b-4 border-gray-500 text-center">
+          Contact details
+        </h1>
+
+        <div className="form w-full sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] mb-[200px] sm:mb-20">
+          <h3 className="font-semibold text-gray-700 text-xl text-center">
+            GET IN TOUCH
+          </h3>
           <h1 className="font-bold text-4xl text-center">Contact.</h1>
 
-          <form  action="https://getform.io/f/8b1e055d-3ca6-480d-a03a-146e51d26c50"  method="POST">
-            <label htmlFor="">
-              <h2 className="">Your Name</h2>
+          <form
+            action="https://getform.io/f/8b1e055d-3ca6-480d-a03a-146e51d26c50"
+            method="POST"
+          >
+            <label>
+              <h2 className="mt-6">Your Name</h2>
               <input
                 type="text"
                 name="name"
                 placeholder="What's your good name?"
+                className="w-full px-4 py-2 mt-2 border rounded-lg"
                 required
               />
             </label>
 
-            <label htmlFor="">
-              <h2>Your email</h2>
+            <label>
+              <h2>Your Email</h2>
               <input
                 type="email"
                 name="email"
-                placeholder="What's your web address?"
+                placeholder="What's your email address?"
+                className="w-full px-4 py-2 mt-2 border rounded-lg"
                 required
               />
             </label>
 
-            <label htmlFor="">
+            <label>
               <h2>Your Message</h2>
               <textarea
                 name="message"
@@ -38,12 +48,15 @@ function Contact() {
                 cols="30"
                 rows="10"
                 placeholder="What you want to say?"
+                className="w-full px-4 py-2 mt-2 border rounded-lg"
                 required
               ></textarea>
             </label>
 
-            <div className="text-center">
-              <button className="btn hover:scale-105 duration-200">Send</button>
+            <div className="text-center mt-4">
+              <button className="btn hover:scale-105 duration-200">
+                Send
+              </button>
             </div>
           </form>
         </div>
